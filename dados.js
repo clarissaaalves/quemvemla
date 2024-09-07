a27356dcb6668e2e0001714936c05ee6948cc93d
clarissaaalves <clariaalves@hotmail.com>
2024-09-07 03:41:32 -0300

    Update dados.js
    ampliação

--- dados.js
+++ dados.js
@@ -1,20 +1,52 @@
 let dados = [
     {
+        // Objeto representando um possível malassombro
         titulo: "Um Gato",
-        descrição: "Se esta escutando barulhos de madrugada, percebendo objetos espalhados pela casa ou enxergando vultos misteriosos a noite, saiba que a probabilidade de ser um gato é alta. Periculosidade: média. O que fazer nessa situação: brincar e fazer carinho, mas antes certifique-se de que não é um gato fantasma ou um gatomem.",
+        // Descrição detalhada do malassombro
+        descrição: "Se esta escutando barulhos de madrugada, percebendo objetos espalhados pela casa ou enxergando vultos misteriosos a noite, saiba que a probabilidade de ser um gato é alta. / PERICULOSIDADE: média / O QUE FAZER NESSA SITUAÇÃO: brincar e fazer carinho, mas antes certifique-se de que não é um gato fantasma ou um gatomem.", // Descrição detalhada do malassombro
+        // Link para mais informações
         link: "https://www.petlove.com.br/gatos/racoes",
-        tags: "vultos barulho barulhos madrugada noite bagunça"
+        // Palavras-chave para facilitar a pesquisa
+        tags: "vultos silhueta borrão barulho barulhos madrugada noite bagunça"
     },
     {
+        // Objeto representando um possível malassombro
         titulo: "O Comodo Sibilante",
-        descrição: "Se escutar um barulho alto e sibilante vir de algum cômodo e quando entrar nele, sentir uma forte corrente de ar na sua direção, saiba que ou existe uma janela aberta no local, ou alguém morreu nesse quarto assobiando. Periculosidade: alta. O que fazer nessa situação: evitar entrar no quarto e se mudar.",
+        // Descrição detalhada do malassombro
+        descrição: "Se escutar um barulho alto e sibilante vir de algum cômodo e quando entrar nele, sentir uma forte corrente de ar na sua direção, saiba que ou existe uma janela aberta no local, ou alguém morreu nesse quarto assobiando. / PERICULOSIDADE: alta / O QUE FAZER NESSA SITUAÇÃO: evitar entrar no quarto e se mudar.",
+        // Link para mais informações
         link: "https://appmeuimovel.com/",
-        tags: "vultos barulho barulhos madrugada noite bagunça quarto"
+        // Palavras-chave para facilitar a pesquisa
+        tags: "assobio vento barulhos madrugada noite bagunça quarto"
     },
     {
+        // Objeto representando um possível malassombro
         titulo: "Você",
-        descrição: "Se existem pessoas saindo e entrando de sua casa com frequência, mas não envolve uma seita satânica, provavelmente a assombração é você. Periculosidade: baixa. O que fazer nessa situação: ir para a luz ou se acostumar a não ter mais que pagar boletos.",
+        // Descrição detalhada do malassombro
+        descrição: "Se existem pessoas saindo e entrando de sua casa com frequência, mas não envolve uma seita satânica, provavelmente a assombração é você. / PERICULOSIDADE: baixa / O QUE FAZER NESSA SITUAÇÃO: ir para a luz ou se acostumar a não ter mais que pagar boletos.",
+        // Link para mais informações
         link: "https://pt.wikihow.com/Ir-para-o-C%C3%A9u-(Cristianismo)",
+        // Palavras-chave para facilitar a pesquisa
         tags: "culto preso trancado pessoas visitantes"
+    },
+    {
+        // Objeto representando um possível malassombro
+        titulo: "Uma Criança",
+        // Descrição detalhada do malassombro
+        descrição: "Se os objetos da sua casa vivem desaparecendo e reaparecendo em outros locais, escuta com frequência o som de alguém correndo pela casa, de gritos e gargalhadas, é provável que seja só uma criança. Tecnicamente, é um tipo diferente de encosto. / PERICULOSIDADE: baixa / O QUE FAZER NESSA SITUAÇÃO: Agora, nada.",
+        // Link para mais informações
+        link: "https://www.pfizer.com.br/noticias/ultimas-noticias/metodos-contraceptivos-vantagens-e-desvantagens",
+        // Palavras-chave para facilitar a pesquisa
+        tags: "choro correria silhueta passos sussurros murmúrios"
+    },
+    {
+        // Objeto representando um possível malassombro
+        titulo: " Lobisomem",
+        // Descrição detalhada do malassombro
+        descrição: "Se ultimamente vem encontrando muitos pelos pela sua casa, ouvindo barulhos estranhos ecoando pelos quartos em dias de lua cheia, percebendo o aparecimento de arranhões inexplicáveis pelo seu corpo, certifique-se se há um gato no recinto, caso contrario deve ser um lobisomem. / PERICULOSIDADE: alta / O QUE FAZER NESSA SITUAÇÃO: trancar as portas e tentar fazer amizade com ele, mas nunca o chame de Totó.",
+        // Link para mais informações
+        link: "https://www.reddit.com/r/furry/comments/13f8fzj/for_furry_friends_you_know_irl_when_do_you_call/?tl=pt-br&rdt=59678",
+        // Palavras-chave para facilitar a pesquisa
+        tags: "pelos uivos ganido latidos luar aluarado furry furries garras arranhões noite lua cheia matilhas licantropo besta lobo toto"
     }
 ];
